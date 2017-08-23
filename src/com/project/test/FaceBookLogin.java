3efd91189b8d12b8c1f1c1308ddd90df1f330936
madhan217 <ks.madhan7@gmail.com>
2017-08-23 06:04:38 +0530

    yes

--- src/com/project/test/FaceBookLogin.java
+++ src/com/project/test/FaceBookLogin.java
@@ -10,8 +10,8 @@ public class FaceBookLogin extends BaseTest {
 	@Test(priority = 1, groups = { "login" })
 	public void testFaceBookLogin() {
 		FacebookPage f = new FacebookPage(driver);
-		f.setEmail("");
-		f.setPass("");
+		f.setEmail("ks.madhan7@gmail.com");
+		f.setPass("skyblue28");
 		f.submitPass();
 
 	}
