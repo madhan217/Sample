274deec9f58d672d08ae5f178cb29830a5947235
madhan217 <ks.madhan7@gmail.com>
2017-08-23 06:20:24 +0530

    save

---
+++ src/com/project/test/Madhan.java
@@ -0,0 +1,13 @@
+package com.project.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Madhan {
+
+	@Test
+	public void openApp()
+	{
+		Reporter.log("Ha ha ha");
+	}
+}
