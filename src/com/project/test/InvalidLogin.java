6ceca8b907ba5137a67492a425a5c495afa99015
madhan217 <ks.madhan7@gmail.com>
2017-08-27 21:27:20 +0530

    x

--- src/com/project/test/InvalidLogin.java
+++ src/com/project/test/InvalidLogin.java
@@ -10,10 +10,10 @@ import generic.BaseTest;
 public class InvalidLogin extends BaseTest{
 	@Test(priority = 1, groups = { "login" })
 	public void testInvalidLogin() {
-		FacebookPage f = new FacebookPage(driver);
-		f.setEmail("ks.madhan7@gmail.com");
-		f.setPass("skyblue");
-		f.submitPass();
+//		FacebookPage f = new FacebookPage(driver);
+//		f.setEmail("ks.madhan7@gmail.com");
+//		f.setPass("skyblue");
+//		f.submitPass();
 		Assert.fail();
 	}
 }
